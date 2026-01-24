3c5c5a8acce74f0ac7c65140245f5fddb1451a34
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-01-24 16:41:02 -0300

    ✨ feat: adiciona estrutura inicial do projeto
    Cria a base da aplicação com pastas públicas, configuração do servidor e organização inicial do código-fonte, incluindo componentes, contextos,
    rotas, telas, estilos, tipos e utilitários.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router'
+import { ContextProvider } from './context'
+import router from './routes'
+import './styles/global.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <ContextProvider>
+            <RouterProvider router={router} />
+        </ContextProvider>
+    </StrictMode>,
 )
