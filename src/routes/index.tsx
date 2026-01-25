40c57acbd6632007998f75a29221543b3adb4833
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-01-25 14:53:57 -0300

    ♻️ refactor: refatora configuração de rotas para usar Layout e rotas aninhadas
    Remove o uso do Wrapper nas rotas e passa a utilizar um Layout com rotas filhas, adotando a API de Component do react-router e
    centralizando a estrutura comum de navegação.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,39 +1,31 @@
-import { createBrowserRouter } from 'react-router';
-import Home from '../screens/Home';
-import { Wrapper } from './helpers';
-import NotFound from '../screens/NotFound';
-import Product from '../screens/Product';
-import Cart from '../screens/Cart';
+import { createBrowserRouter } from 'react-router'
+import Home from '../screens/Home'
+import Layout from '../screens/Layout'
+import NotFound from '../screens/NotFound'
+import Product from '../screens/Product'
+import Cart from '../screens/Cart'
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: (
-			<Wrapper>
-				<Home />
-			</Wrapper>
-		),
-	},
-	{
-		path: '/product/:id',
-		element: (
-			<Wrapper>
-				<Product />
-			</Wrapper>
-		),
-	},
-	{
-		path: '/cart',
-		element: (
-			<Wrapper>
-				<Cart />
-			</Wrapper>
-		),
-	},
-	{
-		path: '*',
-		element: <NotFound />,
-	},
-]);
-
-export default router;
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: Layout,
+        children: [
+            {
+                index: true,
+                Component: Home,
+            },
+            {
+                path: 'product/:id',
+                Component: Product,
+            },
+            {
+                path: 'cart',
+                Component: Cart,
+            },
+        ],
+    },
+    {
+        path: '*',
+        Component: NotFound,
+    },
+])
