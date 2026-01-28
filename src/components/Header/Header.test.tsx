ae1c6e9ed0fd186f81848eb853cce1751a9fdb0c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-01-28 15:20:08 -0300

    :test_tube: test: adiciona testes de navegação no Header
    Atualiza os testes do Header para incluir o uso do MemoryRouter
    e o mock do useNavigate, garantindo que a função de navegação seja disparada corretamente ao clicar no logo.

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,10 +1,36 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router'
 import Header from '.'
+
+const mockNavigate = jest.fn()
+
+jest.mock('react-router', () => ({
+    ...jest.requireActual('react-router'),
+    useNavigate: () => mockNavigate,
+}))
 
 describe('Header', () => {
     test('deve renderizar o header na tela', () => {
-        render(<Header />)
+        render(
+            <MemoryRouter>
+                <Header />
+            </MemoryRouter>,
+        )
 
         expect(screen.getByRole('banner')).toBeInTheDocument()
+    })
+
+    test('deve disparar função de navegação ao clicar no logo', () => {
+        render(
+            <MemoryRouter>
+                <Header />
+            </MemoryRouter>,
+        )
+
+        const logo = screen.getByAltText('logo')
+        fireEvent.click(logo)
+
+        expect(mockNavigate).toHaveBeenCalledWith('/')
+        expect(mockNavigate).not.toHaveBeenCalledWith('/cart')
     })
 })
